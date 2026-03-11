5873d1759d83b6e86d63f3b876060140699fca57
Tobi Alex <tobialex72@gmail.com>
2026-03-11 00:30:30 +0000

    links added

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,8 @@ function Footer() {
   return React.createElement(
     "div",
     { className: "footer" },
-    React.createElement("a", { href: "#" }, "GitHub"),
-    React.createElement("a", { href: "#" }, "Twitter"),
-    React.createElement("a", { href: "#" }, "LinkedIn")
+    React.createElement("a", { href: "https://github.com/OGAlex1" }, "GitHub"),
+    React.createElement("a", { href: "https://x.com/MR_OG_s" }, "Twitter"),
+    React.createElement("a", { href: "https://www.linkedin.com/in/tobi-o-alex?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" }, "LinkedIn")
   );
 }
