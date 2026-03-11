85924e0af02dd8018dccc403bf931b08fc02a356
Tobi Alex <tobialex72@gmail.com>
2026-03-11 00:32:14 +0000

    profile pic

--- src/components/infro.js
+++ src/components/infro.js
@@ -2,7 +2,7 @@ function Info() {
   return React.createElement(
     "div",
     null,
-    React.createElement("img", { src: "profile.jpg", className: "profile-img" }),
+    React.createElement("img", { src: "profile.webp", className: "profile-img" }),
     React.createElement("h1", null, "Paul Richard"),
     React.createElement("h3", null, "Web3 Developer"),
     React.createElement("p", null, "paulrichard.dev"),
