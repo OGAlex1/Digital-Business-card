e289e32c52868f5f7460540b02a476461327404e
Tobi Alex <tobialex72@gmail.com>
2026-03-11 00:22:27 +0000

    about text

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,6 @@ function About() {
     "div",
     { className: "section" },
     React.createElement("h2", null, "About"),
-    React.createElement("p", null, "I am a Web3 developer creating blockchain and web applications.")
+    React.createElement("p", null, "am a frontend developer with experience in Web3 applications, graphic design, and Web3 trading. I create engaging digital experiences that combine technology, design, and blockchain.")
   );
 }
