9e453eead0127203edf1b24a46b8d18683fbc243
Tobi Alex <tobialex72@gmail.com>
2026-03-11 00:11:38 +0000

    footer

---
+++ src/components/Interests.js
@@ -0,0 +1,8 @@
+function Interests() {
+      return React.createElement(
+          "div",
+              { className: "section" },
+                  React.createElement("h2", null, "Interests"),
+                      React.createElement("p", null, "Blockchain, AI, Gaming apps, and Web development.")
+                        );
+                        }
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return React.createElement(
+    "div",
+    { className: "footer" },
+    React.createElement("a", { href: "#" }, "GitHub"),
+    React.createElement("a", { href: "#" }, "Twitter"),
+    React.createElement("a", { href: "#" }, "LinkedIn")
+  );
+}
