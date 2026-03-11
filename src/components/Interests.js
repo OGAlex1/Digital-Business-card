0c8a232c7b18c704a5441866de4c561803adb731
Tobi Alex <tobialex72@gmail.com>
2026-03-11 02:21:00 +0100

    Delete src directory
    Delete

--- src/components/Interests.js
+++
@@ -1,8 +0,0 @@
-function Interests() {
-      return React.createElement(
-          "div",
-              { className: "section" },
-                  React.createElement("h2", null, "Interests"),
-                      React.createElement("p", null, "Blockchain, AI, Gaming apps, and Web development.")
-                        );
-                        }
--- src/components/about.js
+++
@@ -1,8 +0,0 @@
-function About() {
-  return React.createElement(
-    "div",
-    { className: "section" },
-    React.createElement("h2", null, "About"),
-    React.createElement("p", null, "am a frontend developer with experience in Web3 applications, graphic design, and Web3 trading. I create engaging digital experiences that combine technology, design, and blockchain.")
-  );
-}
